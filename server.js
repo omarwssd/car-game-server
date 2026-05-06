978222cbbb1a818b5af156060bff8a074ce1be85
omarwssd <fisheromar49@gmail.com>
2026-05-06 12:52:08 -0500

    server.js

--- server.js
+++ server.js
@@ -6,64 +6,99 @@ const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-const players = {};
+const MAX_PLAYERS = 10;
 
-function broadcast(data) {
+// rooms structure
+const rooms = {
+  // roomId: { players: { id: data }, sockets: { id: ws } }
+};
+
+function createRoom() {
+  const id = "room_" + Math.random().toString(36).substr(2, 6);
+  rooms[id] = { players: {}, sockets: {} };
+  return id;
+}
+
+function findRoom() {
+  for (const id in rooms) {
+    if (Object.keys(rooms[id].players).length < MAX_PLAYERS) {
+      return id;
+    }
+  }
+  return createRoom();
+}
+
+function send(ws, data) {
+  ws.send(JSON.stringify(data));
+}
+
+function broadcast(roomId, data) {
   const msg = JSON.stringify(data);
-  wss.clients.forEach(client => {
+
+  for (const id in rooms[roomId].sockets) {
+    const client = rooms[roomId].sockets[id];
     if (client.readyState === WebSocket.OPEN) {
       client.send(msg);
     }
-  });
+  }
 }
 
 wss.on("connection", (ws) => {
-  const id = Math.random().toString(36).substr(2, 9);
+  const playerId = Math.random().toString(36).substr(2, 9);
+  const roomId = findRoom();
 
-  players[id] = { x: 0, y: 0, z: 0, rot: 0 };
+  ws.roomId = roomId;
+  ws.id = playerId;
 
-  console.log("Player connected:", id);
+  if (!rooms[roomId]) createRoom();
 
-  // Send player their ID
-  ws.send(JSON.stringify({
+  rooms[roomId].players[playerId] = { x: 0, y: 0, z: 0, rot: 0 };
+  rooms[roomId].sockets[playerId] = ws;
+
+  console.log(`Player ${playerId} joined ${roomId}`);
+
+  // send init
+  send(ws, {
     type: "init",
-    id: id,
-    players: players
-  }));
+    id: playerId,
+    room: roomId,
+    players: rooms[roomId].players
+  });
 
-  // Notify others
-  broadcast({
+  // notify others
+  broadcast(roomId, {
     type: "join",
-    id: id,
-    data: players[id]
+    id: playerId,
+    data: rooms[roomId].players[playerId]
   });
 
   ws.on("message", (msg) => {
     const data = JSON.parse(msg);
 
     if (data.type === "update") {
-      players[id] = data.data;
+      rooms[roomId].players[playerId] = data.data;
 
-      broadcast({
+      broadcast(roomId, {
         type: "update",
-        id: id,
+        id: playerId,
         data: data.data
       });
     }
   });
 
   ws.on("close", () => {
-    delete players[id];
+    delete rooms[roomId].players[playerId];
+    delete rooms[roomId].sockets[playerId];
 
-    broadcast({
+    broadcast(roomId, {
       type: "leave",
-      id: id
+      id: playerId
     });
 
-    console.log("Player disconnected:", id);
+    console.log(`Player ${playerId} left ${roomId}`);
   });
 });
 
 server.listen(3000, () => {
-  console.log("Server running on port 3000");
+  console.log("Server running with rooms");
 });
